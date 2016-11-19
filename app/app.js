85ee8e0e495b56ca2ce60a216219f3f4d52dbf62
Methunter <me3sa@me.com>
2016-11-19 12:56:32 +0300

    Stable

--- app/app.js
+++ app/app.js
@@ -1,96 +1,85 @@
-var app = angular.module('myApp', []);
+'use strict';
 
 
-// app.controller('CheckboxController', function($scope) {
-//   $scope.checkboxModel = {
-//     value1 : false,
-//     value2 : 'YES'
-//   }
-// });
-    // article = {"title" = "",
-    // "bigText" = "",
-    // "width" = 0,
-    // "height" = 0,
-    // "pic" = ""};
+var app = angular.module('myApp', ['uploaderApp', 'oi.file']);
+
 var articlesController = function($scope){
-  // size = 10
-  //   $scope.title = "",
-  //   $scope.bigText = "",
-  //   $scope.width = 0,
-  //   $scope.height = 0,
-  //   $scope.pic = ""
-  // rateW = $scope.getRandomInt()
-  // rateH = $scope.getRandomInt()
-  // $scope.width = size 
-  // $scope.height = size
-  // $scope.color = "#f00"
-  // $scope.bgcol = "#00f"
-  // $scope.title = article.title
-  // $scope.p = "Lorem ipsum"
-  // $scope.img = "/Images/pic.gif"
-  // $scope.imgWidth = $scope.width * .9
-  // $scope.imgHeight = $scope.width * .9
-  // $scope.alt = "alt"
-  // $scope.a = "link"
+
 }
 
 app.controller('articlesController', function($scope, $http) {
-  getArticle(); // Load all available articles 
-  function getArticle(){  
-  $http.post("ajax/getArticle.php").success(function(data){
-        $scope.articles = data;
-       });
-  };
 
-  $scope.addArticle = function (title, bigText, width, height, pic) {
-    // var article = {};
-    // article.title = title
-    // article.bigText = bigText
-    // article.width = width
-    // article.height = height
-    // article.pic = pic
-    $http.post("ajax/uploadPic.php")
-    console.log("app.js: "+title+"     "+ bigText+"     "+ width+"     "+ height+"     "+ pic)
-    $http.post("ajax/addArticle.php?title="+title+"&bigText="+bigText+"&width="+width+"&height="+height+"&pic=" + pic).success(function(data){
-        getArticle();
-        $scope.articleInput = "";
-        console.log("data: " + data)
-      });
-        $scope.title = null;
-        title = null;
-  };
-  $scope.deleteArticle = function (article) {
-    if(confirm("Are you sure to delete this article?")){
-    $http.post("ajax/deleteArticle.php?articleID="+article).success(function(data){
-        getArticle();
-      });
-    }
-  };
 
-  $scope.toggleStatus = function(item, status, article) {
-    if(status=='2'){status='0';}else{status='2';}
-      $http.post("ajax/updateArticle.php?articleID="+item+"&status="+status).success(function(data){
-        getArticle();
-      });
-  };
-  $scope.getRandom = function(){
-    // return Math.floor((Math.random()*(3-1)*10));
-    return Math.floor(Math.random() * (10 - 5)) * 50 + 50;
-  }
-  $scope.getRandomInt = function(){
-    // return Math.floor((Math.random()*(3-1)*10));
-  return Math.random() * (3 - 1) + 1;
-  }
- 
+	
+
+
+
+$scope.articleTMPL = "/templates/article.htm"
+$scope.adminTMPL = "/templates/admin.htm"
+$scope.loginTMPL = "/templates/login.htm"
+$scope.picsTMPL = "/templates/pics.htm"
+$scope.checkboxModel = {
+	value1 : true,
+	value2 : 'YES'
+
+}
+$scope.adminka = function(state){
+	if (state == true){
+		$scope.adminVis = true;
+		$scope.artClass = "col-sm-9  col-md-12 col-lg-12  main"
+		$scope.sidebar = "";
+	}else{
+		$scope.artClass = "col-sm-9 col-sm-offset-3 col-md-10 col-lg-10 col-md-offset-2 col-lg-offset-2 main"
+		$scope.adminVis =  false;
+		$scope.sidebar = "sidebar";
+	}
+}
+	getArticle(); // Load all available articles 
+	function getArticle(){  
+		$http.post("ajax/getArticle.php").success(function(data){
+			$scope.articles = data;
+		});
+	};
+
+	$scope.addArticle = function (title, bigText, width, height, pic) {
+
+		$http.post("ajax/uploadPic.php")
+		console.log("app.js: "+title+"     "+ bigText+"     "+ width+"     "+ height+"     "+ pic)
+		$http.post("ajax/addArticle.php?title="+title+"&bigText="+bigText+"&width="+width+"&height="+height+"&pic=" + pic).success(function(data){
+			getArticle();
+			$scope.articleInput = "";
+			console.log("data: " + data)
+		});
+		$scope.title = null;
+		title = null;
+	};
+	$scope.deleteArticle = function (article) {
+		if(confirm("Are you sure to delete this article?")){
+			$http.post("ajax/deleteArticle.php?articleID="+article).success(function(data){
+				getArticle();
+			});
+		}
+	};
+
+	$scope.toggleStatus = function(item, status, article) {
+		if(status=='2'){status='0';}else{status='2';}
+		$http.post("ajax/updateArticle.php?articleID="+item+"&status="+status).success(function(data){
+			getArticle();
+		});
+	};
+	$scope.getRandom = function(){
+		// return Math.floor((Math.random()*(3-1)*10));
+		return Math.floor(Math.random() * (10 - 5)) * 50 + 50;
+	}
+	$scope.getRandomInt = function(){
+		// return Math.floor((Math.random()*(3-1)*10));
+		return Math.random() * (3 - 1) + 1;
+	}
+
+
 
 });
 
-app.controller('checkboxController', function($scope) {
-  $scope.checkboxModel = {
-    value1 : true,
-    value2 : 'YES'
-  }
-});
 
 app.controller('updateController', function($scope){
 
